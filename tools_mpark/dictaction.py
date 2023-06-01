e65c1d4b9395f9e679249f2e2cf69670a2d5689a
Minho Park <m.park@kaist.ac.kr>
2023-06-01 13:30:57 +0000

    useage

--- tools_mpark/dictaction.py
+++ tools_mpark/dictaction.py
@@ -139,3 +139,14 @@ class DictAction(Action):
             d[subkey] = v
 
         setattr(namespace, self.dest, option_cfg_dict)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_add', action=DictAction, nargs='+', default=dict(),
+                        help='override some settings in the used config, the key-value pair '
+                        'in xxx=yyy format will be merged into config file. If the value to '
+                        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+                        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+                        'Note that the quotation marks are necessary and that no white space '
+                        'is allowed.')
